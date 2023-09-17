823519122c6909fa1f0408cf920765cef3eeacc5
Ruth <ruro122020@gmail.com>
2023-09-17 07:41:05 -0400

    display book info on page

--- js/index.js
+++ js/index.js
@@ -1,18 +1,24 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     /***Global Variables */
     let booksList;
     /***Helper functions */
-    function loopThroughBooks(){
+    function loopThroughBooks() {
         booksList.forEach(book => renderBook(book))
     }
     /***Events */
-    function liEventListener(li){
-        li.addEventListener('click', (e)=>{
+    function liEventListener(li) {
+        li.addEventListener('click', (e) => {
             renderBookInfo(li)
         })
     }
+    function clearDOM(parentContainer) {
+        const parentChildren = Array.from(parentContainer.children)
+        if(parentChildren.length){
+            parentChildren.forEach(element => element.remove())
+        }
+    }
     /***Render to DOM */
-    function renderBook(book){
+    function renderBook(book) {
         const booksContainer = document.querySelector('ul#list')
         //create elements
         const li = document.createElement('li')
@@ -22,31 +28,54 @@ document.addEventListener("DOMContentLoaded", function() {
         booksContainer.appendChild(li)
         liEventListener(li)
     }
-    /***Fetch Request */
-    function getBooks(){
-        fetch('http://localhost:3000/books')
-        .then(res => res.json())
-        .then(books => {
-            booksList = books
-            loopThroughBooks()
-        })
-    }
-    function renderBookInfo(li){
+    function renderBookInfo(li) {
+        //parent element to append info to
+        const bookInfoContainer = document.querySelector('div#show-panel')
+        clearDOM(bookInfoContainer)
         //find the book obj of the title that was clicked on
         const liText = li.textContent
         const book = booksList.find(book => book.title === liText)
-        //parent element to append info to
-
+        const likesLiArray = book.users.map(user=>{
+            const userLike = document.createElement('li')
+            userLike.textContent = user.username
+            return userLike
+        })
+       
         //create elements
-
+        const img = document.createElement('img')
+        const title = document.createElement('h2')
+        const subTitle = document.createElement('h2')
+        const author = document.createElement('h2')
+        const description = document.createElement('p')
+        const likeContainer = document.createElement('ul')
         //add texts
-
+        title.textContent = book.title
+        subTitle.textContent = book.subTitle
+        author.textContent = book.author
+        description.textContent = book.description
         //add attributes
-
+        img.src = book.img_url
         //append elements to DOM
+        bookInfoContainer.appendChild(img)
+        bookInfoContainer.appendChild(title)
+        bookInfoContainer.appendChild(subTitle)
+        bookInfoContainer.appendChild(author)
+        bookInfoContainer.appendChild(description)
+        likesLiArray.forEach(element => likeContainer.appendChild(element))
+        bookInfoContainer.appendChild(likeContainer)
     }
+    /***Fetch Request */
+    function getBooks() {
+        fetch('http://localhost:3000/books')
+            .then(res => res.json())
+            .then(books => {
+                booksList = books
+                loopThroughBooks()
+            })
+    }
+
     /***Initialize */
-    function init(){
+    function init() {
         getBooks()
     }
     init()
